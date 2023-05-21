9fc6c0416dc3b41a0b426a4a337b020fcaeb5a2a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-21 00:16:47 -0700

    done

--- custom-writable/customWritable.js
+++ custom-writable/customWritable.js
@@ -1,34 +1,83 @@
-const { Writeable } = require("node:stream/promises");
-const fs = require("node:fs/promises")
+const { Writable } = require("node:stream");
+const fs = require("node:fs");
 
-class FileWriteStream extends Writeable {
-
+class FileWriteStream extends Writable {
   constructor({ highWaterMark, fileName }) {
     super({ highWaterMark });
+
     this.fileName = fileName;
     this.fd = null;
+    this.chunks = [];
+    this.chunksSize = 0;
+    this.writesCount = 0;
   }
-  // runs when constructor is complete; 
-  async _construct(callback) {
-    try {
-      const writeFile = await fs.open(this.fileName, "w")
+
+  // This will run after the constructor, and it will put off all calling the other
+  // methods until we call the callback function
+  _construct(callback) {
+    fs.open(this.fileName, "w", (err, fd) => {
+      if (err) {
+        // so if we call the callback with an argument, it means that we have an error
+        // and we should not proceed
+        callback(err);
+      } else {
+        this.fd = fd;
+        // no argument means it was successful
+        callback();
+      }
+    });
+  }
+
+  // called when writing
+  _write(chunk, encoding, callback) {
+    this.chunks.push(chunk);
+    this.chunksSize += chunk.length;
+
+    if (this.chunksSize > this.writableHighWaterMark) {
+      fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
+        if (err) {
+          return callback(err);
+        }
+        this.chunks = [];
+        this.chunksSize = 0;
+        ++this.writesCount;
+        callback();
+      });
+    } else {
+      // when we're done, we should call the callback function
       callback();
-    } catch (error) {
+    }
+  }
+  // called on last call
+  _final(callback) {
+    fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
+      if (err) return callback(err);
+
+      ++this.writesCount;
+      this.chunks = [];
+      callback();
+    });
+  }
+  // called after stream ends 
+  _destroy(error, callback) {
+    console.log("Number of writes: ", this.writesCount);
+    if (this.fd) {
+      fs.close(this.fd, (err) => {
+        callback(err || error)
+      });
+    } else {
       callback(error);
     }
-
   }
-  _write(chunk, encoding, callback) {
-    // do our write operation
-
-
-
-    // when we're done, we should call the callback 
-    callback();
-  }
-
 }
 
-const stream = new FileWriteStream({ highWaterMark: 1800 })
-stream.write(Buffer.from("Hello this is a string"))
-stream.end(Buffer.from("Out last write."));
+const stream = new FileWriteStream({
+  highWaterMark: 1800,
+  fileName: "test.txt"
+})
+
+stream.write(Buffer.from("HELLO \n"))
+stream.end(Buffer.from("ENDING STREAM"))
+stream.on("finish", () => {
+  console.log("Stream was finished");
+})
